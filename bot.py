b4dcb07181cc171eaabd40de17e6a9d7d96584a9
Epic <admin@itzepic.net>
2020-07-02 11:24:08 +0200

    Cog loader

--- bot.py
+++ bot.py
@@ -4,7 +4,22 @@ Created by Epic at 7/2/20
 import discord
 from discord.ext import commands
 import config
+import logging
+import os
 
 client = commands.Bot("!", allowed_mentions=discord.AllowedMentions(everyone=False, roles=False, users=True))
+logger = logging.getLogger("salbot.core")
+
+client.remove_command("help")
+
+
+@client.event
+async def on_ready():
+    logger.info("Salbot launched")
+    client.help_command = commands.MinimalHelpCommand()
+
+    for cog in os.listdir("cogs/"):
+        client.load_extension("cogs." + cog)
+
 
 client.run(config.token)
