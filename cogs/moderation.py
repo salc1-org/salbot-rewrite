5526c8a4af2568efe4585a02db10aa2e29dfd593
Epic <admin@itzepic.net>
2020-07-02 18:29:59 +0200

    Feat: add kick command (not tested, feel free to verify)

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -33,6 +33,27 @@ class Moderation(commands.Cog):
         else:
             self.logger.error("Error during ban command", exc_info=error)
 
+    @commands.command(aliases=["k", "yeet"])
+    @commands.has_any_role("Administrator", "Moderator")
+    async def kick(self, ctx, user: discord.Member, reason=None):
+        await user.kick(reason=reason)
+        await ctx.message.delete()
+        embed = discord.Embed()
+        embed.colour = 0xFFFF00
+        embed.description = f"Kicked {user.mention}!"
+        await ctx.send(embed=embed)
+
+    @kick.error
+    async def userinfo_error(self, ctx, error):
+        if isinstance(error, commands.errors.BadArgument):
+            await ctx.send("User not found")
+        elif isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.send("Invalid syntax")
+        elif isinstance(error, discord.HTTPException):
+            await ctx.send("I don't have permission to ban this user!")
+        else:
+            self.logger.error("Error during ban command", exc_info=error)
+
 
 def setup(client):
     client.add_cog(Moderation(client))
