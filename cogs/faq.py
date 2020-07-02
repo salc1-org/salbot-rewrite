603776220e7df2257aa6a634d1eb31d5f7941e94
Epic <admin@itzepic.net>
2020-07-02 16:14:59 +0200

    Feat: Add a base faq script

---
+++ cogs/faq.py
@@ -0,0 +1,52 @@
+"""
+Created by Epic at 7/2/20
+"""
+from discord.ext import commands
+import discord
+import logging
+
+
+class Faq(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.logger = logging.getLogger("salbot.faq")
+        self.questions = [
+            {
+                "aliases": ["tos", "terms", "termsofservice"],
+                "text": "Read the discord terms of service at https://discord.com/terms"
+            },
+            {
+                "aliases": ["dgl", "guidelines", "discordguidelines"],
+                "text": "Read the discord guidelines at https://discord.com/guidelines"
+            },
+            {
+                "aliases": ["dupe"],
+                "text": "Please do not mention duping on servers that do not allow it as it's against Discord's ToS."
+            }
+        ]
+        self.mappped_questions = {}
+
+        for question in self.questions:
+            for alias in question["aliases"]:
+                self.mappped_questions[alias] = question["text"]
+
+    @commands.command(aliases=["dgl", "tos"])
+    async def faq(self, ctx, entry=None):
+        if entry is None:
+            description = ""
+            description += "**FAQ Commands:**\n```md\n"
+            for question in self.questions:
+                description += f"+ {question['aliases'][0]}\n"
+            description += "```"
+            await ctx.send(description)
+            return
+        text = ""
+        if entry not in self.mappped_questions.keys():
+            return await ctx.send("Not found.")
+        for splitted in self.mappped_questions[entry].split("\n"):
+            text += f"> {splitted}\n"
+        await ctx.send(text)
+
+
+def setup(client):
+    client.add_cog(Faq(client))
