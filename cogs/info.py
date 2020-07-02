32b0ecb18986e245df725f2cc9fc4851b173873d
Epic <admin@itzepic.net>
2020-07-02 13:16:12 +0200

    Add userinfo command

---
+++ cogs/info.py
@@ -0,0 +1,35 @@
+"""
+Created by Epic at 7/2/20
+"""
+from discord.ext import commands
+import discord
+import typing
+import logging
+
+
+class info(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.logger = logging.getLogger("salbot.info")
+
+    @commands.command(name="userinfo", aliases=["ui", "user"])
+    async def user_info(self, ctx, user: typing.Union[discord.Member, discord.User]):
+        embed = discord.Embed(color=0x00FFFF)
+        embed.set_author(name=str(user), icon_url=user.avatar_url)
+        embed.add_field(name="UserID", value=user.id, inline=True)
+        embed.add_field(name="In server", value="yes" if isinstance(user, discord.Member) else "no")
+        embed.add_field(name="Created at", value=user.created_at.strftime('%A, %d. %B %Y @ %H:%M:%S'), inline=True)
+        if isinstance(user, discord.Member):
+            embed.add_field(name="Joined at", value=user.joined_at.strftime('%A, %d. %B %Y @ %H:%M:%S'), inline=True)
+        await ctx.send(embed=embed)
+
+    @user_info.error
+    async def userinfo_error(self, ctx, error):
+        if isinstance(error, commands.errors.BadUnionArgument):
+            await ctx.send("User not found")
+        elif isinstance(error, commands.errors.MissingRequiredArgument):
+            await self.user_info(ctx, user=ctx.author)
+
+
+def setup(client):
+    client.add_cog(info(client))
