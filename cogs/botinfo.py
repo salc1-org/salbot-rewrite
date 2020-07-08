89262978fcb1e533c0847d94fb682a531d19e6ae
vcokltfre <vcokltfre@gmail.com>
2020-07-08 15:39:24 +0100

    feat: add more stats to bot info

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -13,11 +13,19 @@ class BotInfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.logger = logging.getLogger("salbot.botinfo")
+        self.uptime_start = round(time.time())
         self.stats = {
             "session_messages":0,
-            "session_uptime_start":round(time.time()),
-            "session_commands_completed":0
+            "session_commands_completed":0,
+            "session_member_join":0,
+            "session_member_leave":0
         }
+
+    def event_count(self) -> int:
+        total = 0
+        for key in self.stats:
+            total += self.stats[key]
+        return total
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -27,14 +35,24 @@ class BotInfo(commands.Cog):
     async def on_command_completion(self, ctx):
         self.stats["session_commands_completed"] += 1
 
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        self.stats["session_member_join"] += 1
+    
+    @commands.Cog.listener()
+    async def on_member_leave(self, member):
+        self.stats["session_member_leave"] += 1
+
     @commands.command(name="stats", aliases=["botinfo"])
     @has_any_role("Administrator", "Moderator", "Private Chat Access", "Member")
     async def stats_cmd(self, ctx):
-        uptime = str(timedelta(seconds=round(time.time() - self.stats["session_uptime_start"])))
+        uptime = str(timedelta(seconds=round(time.time() - self.uptime_start)))
 
         stats_embed = discord.Embed(title="SalBot Statistics", color=0x3AE12B, timestamp=datetime.now())
 
-        stats_embed.add_field(name="Uptime", value=uptime, inline=False)
+        stats_embed.add_field(name="Uptime", value=uptime, inline=True)
+        stats_embed.add_field(name="Member Count", value=str(ctx.guild.member_count), inline=True)
+        stats_embed.add_field(name="Events", value=str(self.event_count()), inline=True)
         stats_embed.add_field(name="Messages this Session", value=str(self.stats["session_messages"]), inline=True)
         stats_embed.add_field(name="Commands this Session", value=str(self.stats["session_commands_completed"]), inline=True)
 
