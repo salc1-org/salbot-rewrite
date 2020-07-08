17f56ba24d3d392b71bdb82717c6d339844e0588
vcokltfre <vcokltfre@gmail.com>
2020-07-08 13:22:01 +0100

    chore: rename 'client' to 'bot' and renamed error methods

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -8,8 +8,8 @@ import logging
 
 
 class Moderation(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, bot):
+        self.bot = bot
         self.logger = logging.getLogger("salbot.moderation")
 
     @commands.command(aliases=["b", "plonk"])
@@ -23,7 +23,7 @@ class Moderation(commands.Cog):
         await ctx.send(embed=embed)
 
     @ban.error
-    async def userinfo_error(self, ctx, error):
+    async def ban_error(self, ctx, error):
         if isinstance(error, commands.errors.BadUnionArgument):
             await ctx.send("User not found")
         elif isinstance(error, commands.errors.MissingRequiredArgument):
@@ -44,16 +44,16 @@ class Moderation(commands.Cog):
         await ctx.send(embed=embed)
 
     @kick.error
-    async def userinfo_error(self, ctx, error):
+    async def kick_error(self, ctx, error):
         if isinstance(error, commands.errors.BadArgument):
             await ctx.send("User not found")
         elif isinstance(error, commands.errors.MissingRequiredArgument):
             await ctx.send("Invalid syntax")
         elif isinstance(error, discord.HTTPException):
-            await ctx.send("I don't have permission to ban this user!")
+            await ctx.send("I don't have permission to kick this user!")
         else:
-            self.logger.error("Error during ban command", exc_info=error)
+            self.logger.error("Error during kick command", exc_info=error)
 
 
-def setup(client):
-    client.add_cog(Moderation(client))
+def setup(bot):
+    bot.add_cog(Moderation(bot))
