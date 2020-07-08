d96e1c679bc69710b12f449ad5042d5f8bfa918f
vcokltfre <vcokltfre@gmail.com>
2020-07-08 14:53:56 +0100

    fix: visual improvements to embed

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -6,7 +6,7 @@ from discord.ext.commands import has_any_role
 import discord
 import logging
 import time
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
 class BotInfo(commands.Cog):
@@ -15,22 +15,28 @@ class BotInfo(commands.Cog):
         self.logger = logging.getLogger("salbot.botinfo")
         self.stats = {
             "session_messages":0,
-            "session_uptime_start":round(time.time())
+            "session_uptime_start":round(time.time()),
+            "session_commands_completed":0
         }
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         self.stats["session_messages"] += 1
 
+    @commands.Cog.listener()
+    async def on_command_completion(self, ctx):
+        self.stats["session_commands_completed"] += 1
+
     @commands.command(name="stats", aliases=["botinfo"])
     @has_any_role("Administrator", "Moderator", "Private Chat Access", "Member")
     async def stats_cmd(self, ctx):
         uptime = str(timedelta(seconds=round(time.time() - self.stats["session_uptime_start"])))
 
-        stats_embed = discord.Embed(title="SalBot Statistics")
+        stats_embed = discord.Embed(title="SalBot Statistics", color=0x3AE12B, timestamp=datetime.now())
 
-        stats_embed.add_field(name="Uptime", value=uptime)
-        stats_embed.add_field(name="Messages this Session", value=str(self.stats["session_messages"]))
+        stats_embed.add_field(name="Uptime", value=uptime, inline=False)
+        stats_embed.add_field(name="Messages this Session", value=str(self.stats["session_messages"]), inline=True)
+        stats_embed.add_field(name="Commands this Session", value=str(self.stats["session_commands_completed"]), inline=True)
 
         await ctx.channel.send(embed=stats_embed)
 
