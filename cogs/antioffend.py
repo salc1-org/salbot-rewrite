076edc48fc5f4d2d5ec114e9f24c5513fbe535b3
Epic <admin@itzepic.net>
2020-07-08 21:47:55 +0200

    Feat: Remove offensive nicknames (Feature request by novato)

---
+++ cogs/antioffend.py
@@ -0,0 +1,58 @@
+"""
+Created by Epic at 7/8/20
+"""
+from random import choice
+
+import discord
+from aiohttp import ClientSession
+from discord.ext import commands
+
+import config
+
+
+class AntiOffend(commands.Cog):
+    """Removes nwords from names"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = ClientSession()
+        self.threshold = .5
+        f = open("data/random-names.txt")
+        self.random_names = [i.strip() for i in f.readlines()]
+        f.close()
+
+    async def predict(self, text):
+        payload = {
+            "text": [text]
+        }
+        response = await self.session.post(config.MAX_URL + "/model/predict", json=payload)
+        return (await response.json())["results"][0]["predictions"]
+
+    async def process_name(self, member: discord.Member):
+        prediction = await self.predict(member.display_name)
+        role = discord.utils.get(member.guild.roles, name="Muted")
+        if role in member.roles:
+            return
+
+        for name, value in prediction.items():
+            if value >= self.threshold:
+                await member.edit(nick=choice(self.random_names), reason=f"AntiOffend [{name}]")
+                await member.add_roles(role, reason=f"AntiOffend [{name}]")
+                try:
+                    await member.send(
+                        f"Your nickname in Salc1's discord has been changed. Reason: '{name}'. Please DM a moderator to appeal this nickname change.")
+                except discord.errors.Forbidden:
+                    pass
+                return
+
+    @commands.Cog.listener()
+    async def on_member_add(self, member: discord.Member):
+        await self.process_name(member)
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        await self.process_name(after)
+
+
+def setup(bot):
+    bot.add_cog(AntiOffend(bot))
--- example-config.py
+++ example-config.py
@@ -1,9 +1,10 @@
 """
 Created by Epic at 7/2/20
 
-Fill out all the values and rename this file to example.py
+Fill out all the values and rename this file to config.py
 """
 
 TOKEN = ""
 WEBHOOK_URL = ""
-SENTRY_DSN = ""
+SENTRY_DSN = ""  # https://sentry.io/
+MAX_URL = ""  # https://github.com/IBM/MAX-Toxic-Comment-Classifier
