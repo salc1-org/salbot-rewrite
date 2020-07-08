9af57fbdc7dcf504f90b76c700d6450273280137
vcokltfre <vcokltfre@gmail.com>
2020-07-08 16:22:40 +0100

    feat: update stats to use socket stats (incomplete)

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -7,6 +7,7 @@ import discord
 import logging
 import time
 from datetime import timedelta, datetime
+import json
 
 
 class BotInfo(commands.Cog):
@@ -14,55 +15,33 @@ class BotInfo(commands.Cog):
         self.bot = bot
         self.logger = logging.getLogger("salbot.botinfo")
         self.uptime_start = round(time.time())
-        self.stats = {
-            "session_messages": 0,
-            "session_commands_completed": 0,
-            "session_member_join": 0,
-            "session_member_leave": 0
+        self.socket_stats = {}
+        self.opcodes = {
+            10: "Hello",
+            11: "HEARTBEAT",
+            9: "HI",
+            7: "RECONNECT"
         }
 
-    def event_count(self) -> int:
-        total = 0
-        for key in self.stats:
-            total += self.stats[key]
-        return total
-
     @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        self.stats["session_messages"] += 1
+    async def on_socket_response(self, data):
+        t = data["t"]
+        if not t:
+            try:
+                t = self.opcodes[data["op"]]
+            except KeyError:
+                self.logger.warning(f"Unknown opcode. Received: {data['op']}")
+        self.socket_stats[t] = self.socket_stats.get(t, 0) + 1
 
-    @commands.Cog.listener()
-    async def on_command_completion(self, ctx):
-        self.stats["session_commands_completed"] += 1
-
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        self.stats["session_member_join"] += 1
-
-    @commands.Cog.listener()
-    async def on_member_leave(self, member):
-        self.stats["session_member_leave"] += 1
-
-    @commands.command(name="stats", aliases=["botinfo"])
-    @has_any_role("Administrator", "Moderator", "Private Chat Access", "Member")
-    async def stats_cmd(self, ctx):
-        uptime = str(timedelta(seconds=round(time.time() - self.uptime_start)))
-
-        stats_embed = discord.Embed(
-            title="SalBot Statistics", color=0x3AE12B, timestamp=datetime.now())
-
-        stats_embed.add_field(name="Uptime", value=uptime, inline=True)
-        stats_embed.add_field(name="Member Count", value=str(
-            ctx.guild.member_count), inline=True)
-        stats_embed.add_field(name="Events", value=str(
-            self.event_count()), inline=True)
-        stats_embed.add_field(name="Messages this Session", value=str(
-            self.stats["session_messages"]), inline=True)
-        stats_embed.add_field(name="Commands this Session", value=str(
-            self.stats["session_commands_completed"]), inline=True)
-
-        await ctx.channel.send(embed=stats_embed)
-
+    @commands.command(name="stats")
+    @has_any_role("Administrator", "Moderator")
+    async def stats_bot(self, ctx, typ="raw"):
+        if typ == "raw":
+            jsd = json.dumps(self.socket_stats)
+            desc = f"```json\n{jsd}```"
+            desc = desc.replace(",", ",\n")
+            embed = discord.Embed(title="Raw Socket Stats", color=0xFF0000, description=desc, timestamp=datetime.now())
+            await ctx.channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(BotInfo(bot))
