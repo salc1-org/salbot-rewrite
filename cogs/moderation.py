788ad86f47d1d1e43dff95f6e1e1a2f51a5caf3b
vcokltfre <vcokltfre@pop-os.localdomain>
2020-07-11 23:48:38 +0100

    feat: add basic muting to moderation

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -6,6 +6,9 @@ import typing
 
 import discord
 from discord.ext import commands
+from discord.ext.commands import has_any_role
+
+from helpers.roles import add_role, remove_role
 
 
 class Moderation(commands.Cog):
@@ -14,7 +17,7 @@ class Moderation(commands.Cog):
         self.logger = logging.getLogger("salbot.cogs.moderation")
 
     @commands.command(aliases=["b", "plonk"])
-    @commands.has_any_role("Administrator", "Moderator")
+    @has_any_role("Administrator", "Moderator")
     async def ban(self, ctx, user: typing.Union[discord.Member, discord.User], reason=None):
         await ctx.guild.ban(user, reason=reason)
         await ctx.message.delete()
@@ -35,7 +38,7 @@ class Moderation(commands.Cog):
             self.logger.error("Error during ban command", exc_info=error)
 
     @commands.command(aliases=["k", "yeet"])
-    @commands.has_any_role("Administrator", "Moderator")
+    @has_any_role("Administrator", "Moderator")
     async def kick(self, ctx, user: discord.Member, reason=None):
         await user.kick(reason=reason)
         await ctx.message.delete()
@@ -54,6 +57,46 @@ class Moderation(commands.Cog):
             await ctx.send("I don't have permission to kick this user!")
         else:
             self.logger.error("Error during kick command", exc_info=error)
+
+    # Temporary mute command filler, will be replaced with a more comprehensive system soon
+    @commands.command(name="mute", aliases=["silence"])
+    @has_any_role("Administrator", "Moderator")
+    async def mute(self, ctx, member: discord.Member):
+        success = await add_role(member, "Muted", reason="This user has been muted by staff")
+
+        if success:
+            await ctx.channel.send(f"Successfully muted {member}")
+        else:
+            await ctx.channel.send(f"Couldn't mute {member}")
+
+    # Temporary unmute command filler, will be replaced with a more comprehensive system soon
+    @commands.command(name="mute", aliases=["silence"])
+    @has_any_role("Administrator", "Moderator")
+    async def mute(self, ctx, member: discord.Member):
+        success = await remove_role(member, "Muted", reason="This user has been muted by staff")
+
+        if success:
+            await ctx.channel.send(f"Successfully unmuted {member}")
+        else:
+            await ctx.channel.send(f"Couldn't unmute {member}")
+
+    @mute.error
+    async def mute_error(self, ctx, error):
+        if isinstance(error, commands.errors.BadArgument):
+            await ctx.send("User not found")
+        elif isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.send("Invalid syntax")
+        else:
+            self.logger.error("Error during mute command", exc_info=error)
+
+    @unmute.error
+    async def unmute_error(self, ctx, error):
+        if isinstance(error, commands.errors.BadArgument):
+            await ctx.send("User not found")
+        elif isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.send("Invalid syntax")
+        else:
+            self.logger.error("Error during unmute command", exc_info=error)
 
 
 def setup(bot):
