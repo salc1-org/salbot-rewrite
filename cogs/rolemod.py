9c4749d332cfdf764a956fba0e71ab183325b821
vcokltfre <vcokltfre@pop-os.localdomain>
2020-07-11 23:45:48 +0100

    fix: improve vocalisation of role moderation

--- cogs/rolemod.py
+++ cogs/rolemod.py
@@ -22,12 +22,22 @@ class RoleMod(commands.Cog):
     @rolemod.command(name="add")
     async def rm_add(self, ctx, member: discord.Member, *rolename):
         name = " ".join(rolename)
-        await add_role(member, name, "rolemod")
+        success = await add_role(member, name, "rolemod")
+
+        if success:
+            await ctx.channel.send(f"Successfully added role {name} to {member}")
+        else:
+            await ctx.channel.send(f"Failed to add role {name} to {member}")
 
     @rolemod.command(name="remove", aliases=["rm", "delete"])
     async def rm_remove(self, ctx, member: discord.Member, *rolename):
         name = " ".join(rolename)
-        await remove_role(member, name, "rolemod")
+        success = await remove_role(member, name, "rolemod")
+
+        if success:
+            await ctx.channel.send(f"Successfully removed role {name} from {member}")
+        else:
+            await ctx.channel.send(f"Failed to remove role {name} from {member}")
 
 
 def setup(bot):
