959262b99ff50cbed37a95fa374a40687e06639c
vcokltfre <vcokltfre@pop-os.localdomain>
2020-07-12 00:09:44 +0100

    fix: improve exception handling for roles

--- helpers/roles.py
+++ helpers/roles.py
@@ -1,4 +1,5 @@
 import discord
+from discord import Forbidden, HTTPException
 
 def get_role_by_name(name, guild: discord.Guild):
     for role in guild.roles:
@@ -10,7 +11,9 @@ async def add_role(member: discord.Member, rolename: str, reason=None) -> bool:
     try:
         await member.add_roles(get_role_by_name(rolename, guild), reason=reason)
         return True
-    except:
+    except HTTPException:
+        return False
+    except Forbidden:
         return False
 
 async def remove_role(member: discord.Member, rolename: str, reason=None) -> bool:
@@ -18,5 +21,7 @@ async def remove_role(member: discord.Member, rolename: str, reason=None) -> boo
     try:
         await member.remove_roles(get_role_by_name(rolename, guild), reason=reason)
         return True
-    except:
+    except HTTPException:
+        return False
+    except Forbidden:
         return False
