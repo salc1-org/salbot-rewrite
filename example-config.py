907bc098cd0c716441d8eaca92e2f4c5609805ff
Epic <admin@itzepic.net>
2020-07-11 18:55:19 +0200

    Feat: Report errors to a core server

--- example-config.py
+++ example-config.py
@@ -6,5 +6,4 @@ Fill out all the values and rename this file to config.py
 
 TOKEN = ""
 WEBHOOK_URL = ""
-SENTRY_DSN = ""  # https://sentry.io/
 MAX_URL = ""  # https://github.com/IBM/MAX-Toxic-Comment-Classifier
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ Created by Epic at 7/2/20
 import logging
 
 import sentry_sdk
+import subprocess
 
-import config
 from color_format import colorFormat
 from discord_logger import DiscordFormatter
 
@@ -22,8 +22,10 @@ launcher_logger = logging.getLogger("salbot.launcher")
 launcher_logger.info("Starting salbot")
 
 # Sentry
-if config.SENTRY_DSN != "":
-    sentry_sdk.init(dsn=config.SENTRY_DSN)
+commit_hash = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("utf-8").strip()
+sentry_sdk.init("https://4440e793db8c4d4b810adb8f161980e1@sentry.farfrom.world/2", release=commit_hash)
+launcher_logger.debug(f"Started sentry with commit hash {commit_hash}")
+
 
 while True:
     try:
