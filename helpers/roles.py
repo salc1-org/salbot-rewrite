c344a0689bd914c094899064dd7665527b9d4023
vcokltfre <vcokltfre@pop-os.localdomain>
2020-07-11 23:36:49 +0100

    feat: add helper to add and remove roles

---
+++ helpers/roles.py
@@ -0,0 +1,22 @@
+import discord
+
+def get_role_by_name(name, guild: discord.Guild):
+    for role in guild.roles:
+        if role.name == name:
+            return role
+
+async def add_role(member: discord.Member, rolename: str, reason=None) -> bool:
+    guild = member.guild
+    try:
+        await member.add_roles(get_role_by_name(rolename, guild), reason=reason)
+        return True
+    except:
+        return False
+
+async def remove_role(member: discord.Member, rolename: str, reason=None) -> bool:
+    guild = member.guild
+    try:
+        await member.remove_roles(get_role_by_name(rolename, guild), reason=reason)
+        return True
+    except:
+        return False
