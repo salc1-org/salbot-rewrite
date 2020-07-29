2aee4c8d20121cf88a49ce052ed3ca00545cf087
TAG-Epic <admin@itzepic.net>
2020-07-29 19:18:58 +0200

    Feat: add basic http wrapper

--- cogs/api.py
+++ cogs/api.py
@@ -4,11 +4,10 @@ Created by Epic at 7/14/20
 
 import logging
 
-import discord
 from discord.ext import commands
 from aiohttp import ClientSession
 import config
-from datetime import datetime, timedelta
+from httputils import Route
 
 
 class Api(commands.Cog):
@@ -20,36 +19,17 @@ class Api(commands.Cog):
 
     class ApiException(BaseException):
         def __init__(self, message):
-            pass
+            super().__init__(message)
 
-    async def get_bans(self, *, banned=None, moderator=None):
-        query = {
-            "banned": banned,
-            "moderator": moderator
-        }
-        params = query.copy()
-        for key, value in query.items():
-            if value is None:
-                del params[key]
+    async def request(self, route: Route, **kwargs):
+        full_route = config.API_URL + route.route
+        method = route.method
 
-        response = await self.session.get(f"{config.API_URL}/bans/get", params=params)
-        return await response.json()
+        r = await self.session.request(method, full_route, **kwargs)
+        return await r.json()
 
-    async def add_ban(self, banned, moderator, expires_in: timedelta = None, reason=None):
-        current_time = datetime.utcnow()
-        data = {
-            "id": banned,
-            "moderator": moderator,
-            "banned_at": current_time.timestamp(),
-            "expires_at": (current_time + expires_in).timestamp() if expires_in is not None else None
-        }
-        if data["expires_at"] is None:
-            del data["expires_at"]
-        response = await self.session.put(f"{config.API_URL}/bans/edit", json=data, headers=self.authentication_headers)
-        data = await response.json()
-        if not data["success"]:
-            raise self.ApiException(data["message"])
-        return await response.json()
+
+
 
 
 def setup(bot):
